beee3443a091cbd035c3b04f7a72cd7226bf8a04
AbarnaAnguraj <abarnaanguraj26@gmail.com>
2016-08-19 11:46:18 +0530

    Add files via upload

---
+++ Leastno.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Leastno {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int k=s.nextInt();
+		String a=String.valueOf(n);
+	    char[] b=a.toCharArray();
+	    Arrays.sort(b);
+	    a=new String(b);
+	    StringBuffer sb=new StringBuffer(a);
+	    int c=a.length()-k;
+	    sb.delete(c, a.length());
+	    a=sb.toString();
+	    n=Integer.parseInt(a);
+	    System.out.println(n);
+	}
+
+}
