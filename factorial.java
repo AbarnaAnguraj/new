04bf3914d3441d87aeafdd1c6e08edb684ad29ce
AbarnaAnguraj <abarnaanguraj26@gmail.com>
2016-09-07 11:33:25 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class fact
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=fact(a);
+System.out.println(b);
+}
+public static int fact(int m)
+{
+if(m==1)
+return 1;
+else
+return m*fact(m-1);
+}
+}
