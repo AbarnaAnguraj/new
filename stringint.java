2898f09e2899ac439df78a641d04db1f3613884b
AbarnaAnguraj <abarnaanguraj26@gmail.com>
2016-09-07 11:49:44 +0530

    Add files via upload

---
+++ stringint.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class stringint {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String a=s.next();
+		int b=Integer.parseInt(a);
+		System.out.println(b);
+
+	}
+
+}
